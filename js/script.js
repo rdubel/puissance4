c8ddca9347dfdb76cb02d077af307c39caac2c6d
Rémy <remdub@live.fr>
2017-02-09 17:18:26 +0100

    base game mechanics finished

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 var grid = document.getElementById('grid');
+var tdArray = [];
+tdArray.push(document.getElementsByTagName("td"));
 var slotArray;
+var turn = "r";
+
 
 function generateGrid(x, y) {
     var tab = [];
@@ -12,29 +16,54 @@ function generateGrid(x, y) {
     return tab;
 }
 
-function print(grd, tab) {
+function print() {
+    while (grid.firstChild) {
+        grid.removeChild(grid.firstChild);
+    }
     var th = document.createElement("thead");
-    grd.appendChild(th);
-    for(var i=0; i<tab[0].length; i++) {
+    grid.appendChild(th);
+    for(var i=0; i<slotArray[0].length; i++) {
         var btn = document.createElement("th");
         btn.setAttribute("onclick", "placeChip("+i+")");
         th.appendChild(btn);
     }
 
-    for (i=0; i<tab.length; i++){
+    for (i=0; i<slotArray.length; i++){
         var row = document.createElement("tr");
-        grd.appendChild(row);
-        for (j=0; j<tab[i].length; j++) {
+        grid.appendChild(row);
+        for (j=0; j<slotArray[i].length; j++) {
             var col = document.createElement("td");
+            if (slotArray[i][j] == "r") {
+                col.className = "red";
+            }
+            if (slotArray[i][j] == "y") {
+                col.className = "yellow";
+            }
             row.appendChild(col);
         }
     }
 }
 
 function placeChip(x) {
-    console.log(x);
+    var chip = 1;
+    for (var i = slotArray.length - 1; i >= 0; i--) {
+        if (chip != 0) {
+            if (slotArray[i][x] == "") {
+                slotArray[i][x] = turn;
+                print(grid, slotArray);
+                chip = 0;
+                if (turn == "y") {
+                    turn = "r";
+
+                } else {
+                    turn = "y";
+                }
+            }
+        }
+    }
+
 }
 
 slotArray = generateGrid(6, 7);
 print(grid, slotArray);
-console.log(slotArray);
+console.log(tdArray);
