561d3def936aac389d6c716ab118d197b4133f87
ThibDT <thibault.de.tourdonnet@gmail.com>
2017-02-09 15:56:51 +0100

    something

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
-var board = document.getElementById('board');
-var grid = document.getElementById('grille');
+var grid = document.getElementById('grid');
 var slotArray;
 
 function generateGrid(x, y) {
@@ -13,5 +12,29 @@ function generateGrid(x, y) {
     return tab;
 }
 
+function print(grd, tab) {
+    var th = document.createElement("thead");
+    grd.appendChild(th);
+    for(var i=0; i<tab[0].length; i++) {
+        var btn = document.createElement("th");
+        btn.setAttribute("onclick", "placeChip("+i+")");
+        th.appendChild(btn);
+    }
+
+    for (i=0; i<tab.length; i++){
+        var row = document.createElement("tr");
+        grd.appendChild(row);
+        for (j=0; j<tab[i].length; j++) {
+            var col = document.createElement("td");
+            row.appendChild(col);
+        }
+    }
+}
+
+function placeChip(x) {
+    console.log(x);
+}
 
 slotArray = generateGrid(6, 7);
+print(grid, slotArray);
+console.log(slotArray);
